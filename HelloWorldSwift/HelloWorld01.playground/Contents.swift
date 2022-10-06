17d3299bd3f78494bcbf08f8dbe72e1777f93317
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-06 21:20:21 +0700

    simpler way to include values in strings
    - write a backslash (\) before the parentheses

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -5,11 +5,16 @@ import UIKit
 let apples = 3
 let oranges = 5
 
+let float1 = 2.0
+let float2 = 3.0
+let name = "Hello"
+
 // Write the value in parentheses and write a backslash (\) before the parentheses.
 
 let appleSummary = "I have \(apples) apples."
 
 let fruitSummary = "I have \(apples + oranges) pieces of fruit."
 
-print(fruitSummary)
+let resultFloat = "sum \(float1 + float2) \(name)"
 
+print(resultFloat)
