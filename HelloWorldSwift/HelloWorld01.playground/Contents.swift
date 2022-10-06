0c286e42b7683012d82f0fe8d59bb3840f561aa7
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-06 21:00:10 +0700

    Values are never implicitly converted to another type.

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -1,10 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
 
-var myVariable = 42 // var to make a variable.
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+let explicitFloat: Float = 4.2
 
-myVariable = 50
-
-let myConstant = 42 // let to make a constant
-// The value of a constant doesn't need to be known at compile time
+// Values are never implicitly converted to another type.
+let label = "The width is "
+let width = 94
+// make an instance of the desired type.
+let widthLabel = label + String(width)
