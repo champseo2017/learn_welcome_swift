57aa99e9cb44ef22ad7dee2efdf59e7b4428b72e
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-07 09:37:04 +0700

    Control Flow
    - Use if and switch to make conditionals
    - Use for-in
    - Write a question mark (?)

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -1,7 +1,33 @@
 import UIKit
 
 
-// To create an empty array or dictionary, use the initializer syntax.
+// Control Flow
+// Use if and switch to make conditionals
+// Use for-in, for, while and do-while to make loops.
+// Parentheses around the condition or loop variable are optional.
+// Because around the body are required.
 
-var emptyArray = [String]()// array key string empty string
-var emptyDictionary = Dictionary<String, Float>() // array key value Dictionary<String, Float>
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+
+// Write a question mark (?) after the type of a value to mark the value as optional
+// An optional value either contains a value or contains nil to indicate the value is missing.
+
+var optionalString: String? = "Hello"
+optionalString == nil
+
+var optionalName: String? // nil
+var greeting = "Hello!"
+
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+print(greeting)
