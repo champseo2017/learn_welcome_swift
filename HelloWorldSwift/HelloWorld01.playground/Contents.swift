bb42299450a70545517ab90a29bac89941f35799
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-08 11:34:17 +0700

    Functions and Closures
    - Use a tuple to return multiple values from a function.

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -3,12 +3,12 @@ import UIKit
 
 // Functions and Closures
 
-// Use func to declare a functions.
-// Call a function by following its name with a list of arguments in parentheses.
-// Use -> to separate the parameter names and types from the function's return type.
+// Use a tuple to return multiple values from a function.
 
-func greet(name: String, toDay: String) -> String {
-    return "Hello \(name), today is \(toDay)"
+func getGasPrices() -> (Double, Double, Double) {
+    
+    return (3.59, 3.69, 3.79)
+    
 }
 
-greet(name: "Bob", toDay: "Tuesday")
+getGasPrices()
