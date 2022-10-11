66d3cde5f487432250ca9a04b599f5dcea137327
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-11 09:57:28 +0700

    Functions are actually a special case of closures
    - Use in to separate the arguments and return type from the body
    https://stackoverflow.com/questions/24056985/how-to-know-if-a-number-is-odd-or-even-in-swift

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -1,21 +1,18 @@
 import UIKit
 
 
-// A function can take another funtion as one of its arguments
-
-func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
-    for item in list {
-        if condition(item) {
-            return true
-        }
-    }
-    return false
-}
-
-func lessThanTen(number: Int) -> Bool {
-    return number < 10
-}
+// Functions are actually a special case of closures
+// Use in to separate the arguments and return type from the body
 
 var numbers = [20, 19, 7, 12]
 
-hasAnyMatches(list: numbers, condition: lessThanTen)
+let result = numbers.map({
+    (number: Int) -> Int in
+    if number % 2 == 0 {
+       return number // is even number
+      } else {
+       return 0 // is odd number
+    }
+})
+
+print(result)
