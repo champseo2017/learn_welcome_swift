f9ccc57871eb080c7ac57360b07dafaf778dd7d4
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-11 09:30:29 +0700

    Functions are a first-class type.
    - This means that a function can return another function as its value

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -1,25 +1,17 @@
 import UIKit
 
 
-// Functions and Closures
+// Functions are a first-class type.
+// This means that a function can return another function as its value
 
-// Functions can be nested.
-// Nested functions have access to variables that were declared in the outer function.
-
-// You can use nested functions to organize the code in a function that is long or complex.
-
-func returnFifteen() -> Int {
-    
-    var y = 10
-    
-    func add() {
-        y += 5
+func makeIncrementer() -> ((Int) -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
     }
-    
-    add()
-    
-    return y
-    
-    
+    return addOne
 }
-returnFifteen()
+
+var increment = makeIncrementer()
+
+print(increment(7))
+
