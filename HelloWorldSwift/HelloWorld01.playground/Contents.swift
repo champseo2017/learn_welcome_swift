d5bb13e4f3a79811860ae4134c7cd6780be1ccd3
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-14 18:04:44 +0700

    แสดงข้อความด้วยคำสั่ง print()

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -1,35 +1,39 @@
 import UIKit
 
 
-// Object and class
-
 /*
- เมธอดของคลาส
+  
+ แสดงข้อความด้วยคำสั่ง print()
+ - print() ใน swift จะขึ้นบรรทัดใหม่โดยอัตโนมัติ
+ - สามารถพิมพ์ข้อมูลหลายๆ อย่างในคำสั่ง print() อันเดียวกันได้
+ โดยคั่นข้อมูลเหล่านั้นด้วยเครื่องหมาย คอมม่า , ข้อมูลที่แสดงออกไปจะถูกคั่นด้วยช่องว่าง 1 ช่อง
+ 
+ var s = "Swift language"
+ var c = "Chris Lattner"
+ print(s, "create by", c)
+ 
+ - separator ระบุสิ่งที่จะใช่คั้นระหว่างข้อความ ไว้ในลำดับสุดท้าย
+ 
+ - terminator label ที่ทำให้ข้อมูลพิมพ์อยู่ในบรรทัดเดียวกัน
+    - กำหนด label terminator และ เพิ่มช่องว่างในเครื่องหมายคำพูด
+ 
+ - "\n" ขึ้นบรรทัดใหม่
  
  */
 
-class Random {
-    
-    func integer(bound: UInt32) -> UInt32 {
-        return arc4random_uniform(bound)
-    }
-    
-    func range(min: UInt32, max: UInt32) -> UInt32? {
-        if max >= min {
-            return min + arc4random_uniform(max - min + 1)
-        } else {
-            return nil
-        }
-    }
-    
-    func double() -> Double {
-        return drand48()
-    }
-    
-}
 
+print(31, 12, 2561, separator: "-")
 
-let random = Random() // สร้างอินสแตนซ์ของคลาส Random
-let a = random.integer(bound: 100)
-print(a)
-print(random.range(min: 10, max: 20)!)
+var a = "Apple Swift"
+var s = "Suzuki Swift"
+var t = "Taylor Swift"
+print(a, s, t, separator: ", ")
+
+// terminator
+print("Apple Products", terminator: " ")
+print("iPhone", terminator: " ")
+print("iPad", terminator: " ")
+print("MacBook")
+
+// "\n"
+print("Apple Products: \niPhone \niPad \nMacBook")
