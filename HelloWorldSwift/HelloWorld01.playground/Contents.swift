6a48ca658193b77b4489700a79fd0009292f7fa9
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-14 17:51:44 +0700

    Object and class
    - เมธอดของคลาส

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -4,21 +4,32 @@ import UIKit
 // Object and class
 
 /*
- การนำคลาสไปใช้งาน ต้องสร้าง (Instance) หรือ ออบเจ็กต์ (Object)
- ของมันขึ้นมาเสียก่อน แล้วเรียกใช้สมาชิกผ่านทางอินสแตนซ์นี้
- 
- var person: Person
- person = Person()
- 
- let random: RandomNumber = RandomNumber()
- let thDate: ThaiDate = ThaiDate()
- 
- // หรือใช้รูปแบบ Type Inference เช่น
- var person = Person()
- let random = RandomNumber()
- let thDate = ThaiDate()
- 
- 
- 
+ เมธอดของคลาส
  
  */
+
+class Random {
+    
+    func integer(bound: UInt32) -> UInt32 {
+        return arc4random_uniform(bound)
+    }
+    
+    func range(min: UInt32, max: UInt32) -> UInt32? {
+        if max >= min {
+            return min + arc4random_uniform(max - min + 1)
+        } else {
+            return nil
+        }
+    }
+    
+    func double() -> Double {
+        return drand48()
+    }
+    
+}
+
+
+let random = Random() // สร้างอินสแตนซ์ของคลาส Random
+let a = random.integer(bound: 100)
+print(a)
+print(random.range(min: 10, max: 20)!)
