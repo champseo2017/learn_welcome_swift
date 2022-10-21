3ad080ca3ef6a7196686a06801ef59b61071511d
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-21 09:29:27 +0700

    การเขียนอักขระพิเศษบางตัวในสตริง

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -3,29 +3,20 @@ import UIKit
 
 /*
   
- การแทรกข้อมูลลงในสตริง
+ การเขียนอักขระพิเศษบางตัวในสตริง
+ 
  
  
  
  */
 
- let no = 5
- let str = "The winner is number \(no)"
+ var str = "Model: iPhone XX \n"
+ str += "Display: 5.67\""
+ print(str)
 
-let name = "Mr. John"
-let country = "UK"
-let msg = "Number \(no) is \(name) from \(country)"
-print(msg)
 
-let a = 2
-let b = 1
-
-print("Arsenal \(a):\(b) Barcelona")
-
-let t: Bool = true
-print("Vision with Action make your dream come \(t)")
-
-let x: Int = 10
-let y: Int = 20
-
-print("\(x) + \(y) = \(x + y)")
+ let a = 10
+ print("a = \(a)")
+ print("a = \\(a)")
+ print("a = \\\(a)")
+ print("one\ttwo\tthree")
