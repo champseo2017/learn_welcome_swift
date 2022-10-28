537b9219c11d7dd515e5088d5378b76035d5cad7
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-10-28 09:22:45 +0700

    การกำหนดช่วงข้อมูลด้วย Range Operator
    - Half-Open Range Operator

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -5,30 +5,26 @@ import UIKit
   
  การกำหนดช่วงข้อมูลด้วย Range Operator
  
-  - Closed Range Operator
-    - กำหนดค่าแรกและค่าสุดท้ายของช่วงข้อมูลด้วย 3 จุด
-    a...b
-    - ค่าแรกต้องน้อยกว่า ค่าที่สอง
-    - นับรวมค่าแรกและค่าสุดท้ายใน Range Operator ด้วย
-    - 1...10 (1, 2, 3,..., 10)
-    - 50...99 (50 - 59)
-    - 1000...100 Error
-    - -10...(-1) ค่าสุดท้ายติดลบเขียนไว้ในวงเล็บ (ค่าแรกจะใส่วงเล็บหรือไม่ก็ได้)
+  - Half-Open Range Operator
+    - กำหนดช่วงโดยใช้ 2 จุดตามด้วยเครื่องหมาย <
+    a..<b
+    - นับจากค่าแรกเป็นต้นไป แต่ไม่นับค่าสุดท้าย
+    - 1..<10 (1 - 9 (1, 2, 3, ..., 9))
+    - 50..<99 (50 - 98)
+    - 1000..<100 Error
+    - -10..<(-1) (-10, -9, ..., -1)
  
  
  */
 
-let data = 10...20
-if data.contains(12) {
-    print("12 อยู่ในช่วงที่กำหนด")
-} else {
-    print("12 ไม่อยู่ในช่วงที่กำหนด")
-}
-print(data.count) // 11
-print(data.lowerBound) // 10
-print(data.upperBound) // 20
 
-// let min = 100
-// let max = 150
-// let range = min...max
-// if range.contains(120) {}
+let data = 10..<20
+if data.contains(20) {
+    print("20 อยู่ในช่วงที่กำหนด")
+} else {
+    print("20 ไม่อยู่ในช่วงที่กำหนด")
+}
+print(data.count)
+print(data.lowerBound)
+print(data.upperBound)
+print(data.max()!) // 19
