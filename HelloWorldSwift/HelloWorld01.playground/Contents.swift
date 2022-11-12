02dafc8964488182be7b2b20ba64e4acb28560b4
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-11-12 21:36:35 +0700

    ค่า nil และ Optional Type

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -3,30 +3,21 @@ import UIKit
 
 /*
   
-   การคำนวณหาค่าดัชนีมวลกาย
-   bmi = weight / height กำลัง 2
+   ค่า nil และ Optional Type
+   - nil ที่หมายถึง ไม่มีค่าอะไรเลย ซึ้งเทียบเท่ากับคำว่า null
+   - ไม่มีค่าใดๆ
+   - ไม่เท่ากับ 0
+   - ไม่เท่ากับสตริงว่าง ("")
+   - ไม่เท่ากับ false
+   - ไม่ตรงกับชนิดข้อมูลใดๆ
  
-   bmi < 18.5 ผอม
-   30..<40 อ้วน
-   18.5..<25 พอดี
-   bmi >= 40 อ้วนเกินไป
-   25..<30 เริ่มอ้วน
+   ในภาษา Swift เราไม่สามารถนำค่า null ไปกำหนดให้กับตัวแปร หรือ ค่าคงที่ใดๆ โดยตรงได้
+   เพราะ nil ไม่ตรงกับชนิดข้อมูลใด
+ 
+   let a = nil // Error
+   let b: Int = nil // Error
+   var c: String = "Hello"
+   c = nil // Error
  
  
  */
- 
-let weight: Double = 75
-let height = 1.70
-let shape: String
-let bmi = weight / (height * height)
-
-switch bmi {
- case _ where bmi < 18.5: shape = "ผอมมาก"
- case 18.5..<25: shape = "พอดี"
- case 25..<30: shape = "เริ่มอ้วน"
- case 30..<40: shape = "อ้วน"
- case _ where bmi >= 40: shape = "อ้วนเกินไป"
- default: shape = "ไม่สามารถระบุได้"
-}
-
-print("ลักษณะรูปร่างของคุณ: \(shape)")
