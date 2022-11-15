b869517ff71da454d2d2145722ea96f94c675253
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-11-15 17:46:22 +0700

    Implicitly Unwrapped Optional Type

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -3,42 +3,13 @@ import UIKit
 
 /*
  
- การกำหนดค่าดีฟอลต์แบบ nil coalescing
- - ?? "ค่าดีฟอลต์"
+ Implicitly Unwrapped Optional Type
+ - การกำหนด Type ไม่ใช้มีค่าเป็น nil อย่างแน่นอน
  
- let a: Int? = nil
- var b: Int
- b = a ?? 0 // ถ้าค่า a เป็น nil ใช้ค่าดีฟอลต์ 0 แทน
- print(b)
-  
+ var a: Int! = nil
+ a = 123
+ print(a ?? 0)
   
  */
 
 
-let a: Int? = nil
-var b: Int
-
-if a == nil { // เทียบเท่ากับ
-    b = 0
-}
-
-
-let c: Double? = 1.23
-let d = c ?? -1
-
-var f: Float? = nil
-f = f ?? 0.0
-
-let x: Int? = 10
-let y: Int? = nil
-let z: Int
-
-z = (x ?? 0) + (y ?? -1)
-print(z)
-
-
-let sum = (Int("123") ?? 0) + (Int("xxx") ?? -1)
-print(sum)
-
-let msg: String? = nil
-print(msg ?? "Error")
