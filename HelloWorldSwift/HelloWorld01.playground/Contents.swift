32e7194d9ac9e687dec0d61774eb238b10bb36e8
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-11-15 17:33:02 +0700

    การใช้วิธี Forced Unwrapping

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -3,67 +3,40 @@ import UIKit
 
 /*
   
-  การใช้วิธี Optional Binding
-  - ใช้คำสั่ง if สร้างค่าคงที่หรือตัวแปรที่เท่ากับตัวแปรแบบ Optional แล้วกำหนดทางเลือก
+  การใช้วิธี Forced Unwrapping
+  - ใช้เครื่องหมาย ! ไว้หลังตัวแปรแบบ Optional แต่ต้องแน่ใจว่าตัวแปรนั้นมีค่า
  
-  if <let หรือ var> ตัวแปร = ตัวแปรแบบ Optional {
-   คำสั่ง ในกรณีที่มีค่า
-  } else {
-   คำสั่งต่างๆ กรณีตัวแปรมีค่าเป็น nil
-  }
+ var x: Double? = nil
+ print(x!) // Error เพราะมีค่าเป็น nil
  
-  - ตัวแปรใน if ไม่ใช้การเปรียบเทียบ แต่เป็นการกำหนดค่าให้กับตัวแปร
- 
- let a: Int? = 10
- if let x = a {
-     print("a = \(x)")
- } else {
-     print("a = nil")
- }
- 
- let a: Int? = 10
- if var x = a {
-     print("a = \(x)")
-     x *= 2
-     print("2a = \(x)")
- } else {
-     print("a = nil")
- }
- 
- - การเป็นค่าจากการทำ Optional
- let a: Int? = 10
- var a2: Int
+ var a: Int? = 123
+ a! += 1
+ print(a!)
 
- if let x = a {
-     a2 = x
- } else {
-     a2 = 0
- }
+ let b: String? = "Hello"
+ print(b!)
 
- print("a2 = \(a2)")
- a2 += 1
- 
- var v: Int = 0
- if let temp = Int("123") { // Int() คืนค่าเป็น Optional
-     v = temp
- }
- print(v)
- 
- - ใช้ตัวแปรแบบ Optional หลายๆ ตัวพร้อมกัน สามารถทำการ Binding ในคำสั่ง if เดียวกันได้
- โดยใช้ , คอมม่าคั้นการกำหนดตัวแปรด้วยเครื่องหมาย ,
- 
- - การทำ Optional Binding ในคำสั่ง if หากตัวแปรมีค่าเป็น nil เทียบเท่ากับเงื่อนไขเป็นเท็จ คำสั่งในบล็อก if จะไม่ถูกดำเนินการ
- จะพิมพ์คำว่า Error ออกมา
- 
- let a: Int? = 10
- let b: Int? = 20
- let c: Int? = nil
+ let c: Int? = 10
+ let d: Int? = 20
+ let e: Int? = c! + d!
+ print(e!)
 
- if let x = a, let y = b, var z = c {
-     z = x + y
-     print(z)
+
+ var str: String? = nil
+ str = "Hi"
+ print(str!)
+ 
+ - ไม่ Forced Unwrapping โดยตรง แต่ตรวจสอบด้วย if ก่อนจึงค่อย Forced Unwrapping
+ let x: Double? = nil
+ if x != nil {
+     print(x!)
  } else {
      print("Error")
+ }
+ 
+ let str: String? = nil
+ if str != nil {
+     print(str!)
  }
  
  */
