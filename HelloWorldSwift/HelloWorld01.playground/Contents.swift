8c55c942777173be3f1549ac03b299b9fa200dba
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-11-15 16:55:12 +0700

    การกำหนด Optional Type
    - วิธีตรวจสอบเมื่อตัวแปรเป็น Optional Type

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -3,21 +3,40 @@ import UIKit
 
 /*
   
-   ค่า nil และ Optional Type
-   - nil ที่หมายถึง ไม่มีค่าอะไรเลย ซึ้งเทียบเท่ากับคำว่า null
-   - ไม่มีค่าใดๆ
-   - ไม่เท่ากับ 0
-   - ไม่เท่ากับสตริงว่าง ("")
-   - ไม่เท่ากับ false
-   - ไม่ตรงกับชนิดข้อมูลใดๆ
+  การกำหนด Optional Type
+  
+  var a: Int?
+  var b: Double?
+  let c: String?
+  
+  var a: Int? = 123
+  var b: Double? = 4.56
+  let c: String? = nil
+  
+  a = nil
+  if a == nil {
+    print("a = nil")
+  }
  
-   ในภาษา Swift เราไม่สามารถนำค่า null ไปกำหนดให้กับตัวแปร หรือ ค่าคงที่ใดๆ โดยตรงได้
-   เพราะ nil ไม่ตรงกับชนิดข้อมูลใด
+  b = nil
+  if b == nil {
+   print("b = nil")
+  }
  
-   let a = nil // Error
-   let b: Int = nil // Error
-   var c: String = "Hello"
-   c = nil // Error
+  (c == nil) ? print("c = nil") : print("c != nil") // c = nil
  
+  นำตัวแปรแบบ Optional ไปใช้งานโดยที่ไม่ตรวจสอบก่อนจะเกิด Error ได้
+ 
+  var a: Int?
+  a = 10
+  a += 1 // Error เพราะ a เป็น Optional Type จะใช้งานโดยตรงไม่ได้
+ 
+ 
+  วิธีตรวจสอบเมื่อตัวแปรเป็น Optional Type
+   
+   - Optional Binding การใช้คำสั่ง if ตรวจสอบว่าตัวแปรมีค่าเป็น nil หรือไม่
+   - Force Unwrapping ยกเลิกการตรวจสอบตัวแปรที่เป็น Optional โดยใช้ !
+   - Nil Coalescing กำหนดค่า default เมื่อตัวแปรมีค่าเป็น nil
+   - guard-else คำสั่งที่ป้องกันและหยุดการทำงานของฟังก์ชัน เมื่อมีข้อผิดพลาด
  
  */
