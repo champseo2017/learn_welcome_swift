222cb3b8de71078f76a6feb4d2937cb2b1258146
ChampKos <32265715+champseo2017@users.noreply.github.com>
2022-12-02 09:37:12 +0700

    ฟังก์ชันพื้นฐานเกี่ยวกับสตริง
    - การสร้างสตริงแบบกำหนดค่าตัวแปร
    - การสร้างสตริงด้วยคำสั่ง String()
    - isEmpty
    - append("สตริง") (ไม่คืนค่ากลับมานำไปใช้งานได้เลย)
    - appending("สตริง")
    - count
    - hasPrefix("สตริง")
    - hasSuffix("สตริง")
    - contains("สตริง")
    - uppercased()
    - lowercased()
    - capitalized
     - trimmingCharacters()
    - reversed()
    - prefix(จำนวน) อ่านอักขระเริ่มต้นตามจำนวนที่ระบุ
    - suffix() อ่านอักขระตอนท้ายตามจำนวนที่ระบุ
    - dropFirst(จำนวน)
    - dropLast(จำนวน)
    - split(separator: "อักขระ 1 ตัว")
    - components(separatedBy: "สตริง")

--- HelloWorldSwift/HelloWorld01.playground/Contents.swift
+++ HelloWorldSwift/HelloWorld01.playground/Contents.swift
@@ -86,7 +86,93 @@ if word.contains("no") {
     print("no")
 }
  
+ - uppercased()
+   เปลี่ยนอักขระเป็นตัวพิมพ์ใหญ่ทั้งหมด
+ - lowercased()
+   เปลี่ยนอักขระเป็นตัวพิมพ์เล็กทั้งหมด
+ - capitalized
+   เปลี่ยนอักขระตัวแรกของคำเป็นตัวพิมพ์ใหญ่
+ var th = "thailand"
+ th = th.uppercased()
+ print(th)
+
+ let uk = "United Kingdom"
+ print(uk.lowercased())
+ 
+ let usa = "united states of america"
+print(usa.capitalized)
+ 
+ - trimmingCharacters()
+   - ตัดช่องว่างและอักขระพวก White space เช่น \n, \t ที่อยู่ก่อนและหลังสตริงออก
+     อยู่ภายในสตริงจะไม่ถูกตัด
+ var str = "  Hello World  "
+print("Before trimming num chars = \(str.count)")
+
+str = str.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+print("After trimming num chars = \(str)")
+ 
+ reversed()
+ - เรียงลำดับอักขระในสตริงแบบย้อนกลับ
+   และ ต้องใช้คำสั่ง String() แปลงผลลัพธ์ที่ได้ไปเป็นข้อมูลชนิด String
+ 
+ let str = "ABCDE"
+ let revStr = str.reversed()
+ print(String(revStr))
+ 
+ - prefix(จำนวน) อ่านอักขระเริ่มต้นตามจำนวนที่ระบุ
+   คืนค่ากลับมาต้องแปลงเป็นสตริงด้วยคำสั่ง String()
+ 
+ - suffix() อ่านอักขระตอนท้ายตามจำนวนที่ระบุ
+   คืนค่ากลับมาต้องแปลงเป็นสตริงด้วยคำสั่ง String()
+ 
+ let str = "Hello World"
+ let charView = str.prefix(4)
+ let pref = String(charView)
+ print(pref)
+
+ let suff = String(str.suffix(5))
+ print(suff)
+ 
+ - dropFirst(จำนวน)
+   ตัดอักขระจากเริ่มต้นตามจำนวนที่ระบุ ผลลัพธ์ แปลงเป็น สตริง
+ 
+ - dropLast(จำนวน)
+   ตัดอักขระตอนท้ายตามจำนวนที่ระบุ
+ 
+ let str = "123456"
+var charView = str.dropFirst(3) // ตัด 3 ตัวแรกออก
+let newStr = String(charView)
+print(newStr)
+
+charView = str.dropLast(2) // ตัด 2 ตัวท้ายออก
+print(String(charView))
+ 
+ - dropFirst() นำอักขระตัวแรกของสตริงออกไป
+ - dropLast() นำอักขระตัวสุดท้ายของสตริงออกไป
+ 
+ var str = "ABCDEF"
+var char = str.dropFirst()
+print(String(char))
+print(str)
+
+str = "CVB"
+char = str.dropLast()
+print(String(char))
+print(str)
+ 
+ - split(separator: "อักขระ 1 ตัว") แยกสตริงด้วยอักขระที่ระบุ (ใช้อักขระเพียง 1 ตัวเท่านั้น)
+   ผลลัพธ์ได้เป็น อาร์เรย์
+ - components(separatedBy: "สตริง") แยกสตริงด้วยสตริงย่อยที่ระบุ ผลลัพธ์เป็นอาร์เรย์
+ 
+ let a = "www.google.com"
+let b = a.split(separator: ".") // แยกด้วยจุด
+print(b[1])
+
+let c = "One, Two, Three, Four"
+let d = c.components(separatedBy: ", ") // แยกด้วยคอมม่าและช่องว่าง
+print(d[2])
+ 
  */
  
-
-
+ 
+ 
