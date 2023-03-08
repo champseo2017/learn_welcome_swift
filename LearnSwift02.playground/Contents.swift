11c4f2695ef145058a223a24c523a3324ea2227e
ChampKos <32265715+champseo2017@users.noreply.github.com>
2023-03-08 21:36:19 +0700

    array Type Inference (Int)
    count
    first
    last
    append
    insert
    shuffle

---
+++ LearnSwift02.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var ages = [21, 22] // Type Inference (Int)
+ages.count
+ages.first
+ages.last
+ages[0]
+ages.append(99)
+ages.insert(44, at: 0)
+//ages.sort()
+//ages.reverse()
+ages.shuffle()
+print(ages)
+//var ages: [Int] = []
