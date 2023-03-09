cf1f2cbbfe6ba5f39a87d3d0140b945f923be6c8
ChampKos <32265715+champseo2017@users.noreply.github.com>
2023-03-09 21:02:57 +0700

    Set array

--- LearnSwift02.playground/Contents.swift
+++ LearnSwift02.playground/Contents.swift
@@ -1,14 +1,27 @@
 import UIKit
 
-var ages = [21, 22] // Type Inference (Int)
-ages.count
-ages.first
-ages.last
-ages[0]
-ages.append(99)
-ages.insert(44, at: 0)
-//ages.sort()
-//ages.reverse()
-ages.shuffle()
-print(ages)
-//var ages: [Int] = []
+/*
+ Array
+ Set
+ unordered
+ no duplicates
+ 
+ */
+
+var ages = [13, 33, 55, 17, 33]
+//var agesSet: Set<Int> = []
+
+var agesSet = Set(ages)
+
+
+agesSet.insert(101)
+agesSet.contains(101) // Check array
+print(agesSet)
+
+/*
+ Set
+ - unordered
+ - no duplicates
+ - performance
+ 
+ */
