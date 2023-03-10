ce11bf5eba621793b6a73b97311272914e72e611
ChampKos <32265715+champseo2017@users.noreply.github.com>
2023-03-10 23:30:47 +0700

    switch

--- LearnSwift02.playground/Contents.swift
+++ LearnSwift02.playground/Contents.swift
@@ -1,19 +1,47 @@
 import UIKit
 
 /*
- enum
+ switch
  
  */
 
-enum Phone: String { // Raw value
-  case iPhone11Pro = "a"
-  case iPhoneSE = "b"
-  case pixel = "c"
-  case nokia = "d"
+enum Phone {
+  case iPhone11Pro
+  case iPhoneSE
+  case pixel
+  case nokia
 }
 
 func getSeansOpinion(on phone: Phone) {
-    print(phone.rawValue)
+    switch phone {
+    case .iPhone11Pro:
+        print("a")
+    case .iPhoneSE:
+        print("se")
+    case .pixel:
+        print("pi")
+    default:
+        print("nokia default")
+    }
 }
 
-getSeansOpinion(on: .iPhoneSE)
+//getSeansOpinion(on: .iPhoneSE)
+
+let matchmakingRank = 40
+
+func determinePlayerLeague(from rank: Int) {
+    switch rank {
+    case 0:
+        print("a")
+    case 1..<50:
+        print("b")
+    case 50..<100:
+        print("a")
+    case 100..<200:
+        print("You are")
+    default:
+        print("GGeasy")
+    }
+}
+
+determinePlayerLeague(from: 30)
