2164d91b451f4f0320ad7b1ff90b95b5074eb750
ChampKos <32265715+champseo2017@users.noreply.github.com>
2023-03-10 21:20:08 +0700

    for loop

--- LearnSwift02.playground/Contents.swift
+++ LearnSwift02.playground/Contents.swift
@@ -1,35 +1,24 @@
 import UIKit
 
 /*
- if this, then that
- conditional
- 
- var isDarkModeOn = false
-if isDarkModeOn {
-    print("That's how it should be.")
-} else {
-    print("You are a psycho.")
-}
-
-var myHighScore = 555
-var yourHighScore = 444
-
-if myHighScore > yourHighScore {
-    print("I win")
-} else {
-    print("You win")
-}
- 
+ for loop
  
  */
 
-var highScore = 111
-if highScore > 500 {
-    print("You're the best")
-} else if highScore > 250 {
-    print("GG")
-} else if highScore > 100 {
-    print("wp")
-} else {
-    print("HH")
+let allStars = ["a", "b", "c"]
+
+//for player in allStars where player == "b" {
+//    print(player)
+//}
+
+//for i in 0..<25 {
+//    print(i)
+//}
+
+var randomInts: [Int] = []
+for _ in 0..<25 {
+    let randomNumber = Int.random(in: 0...1000)
+    randomInts.append(randomNumber)
 }
+
+print(randomInts)
